112fb52e1ec8b96601b42c02035f0ffe0b8a17f5
amoungui <samoungui@gmail.com>
2024-03-26 19:27:48 +0100

    do the same thing with the juillet month

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -5,12 +5,13 @@ import { getMonth } from "./index";
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
+        const date = new Date(2022, 0, 1); // Janvier
         it("the function return janvier for 2022-01-01 as date", () => {
-            const date = new Date(2022, 0, 1); // Janvier
             expect(getMonth(date)).toBe('janvier');
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            date.setMonth(6); // Juillet
+            expect(getMonth(date)).toBe('juillet');
         });
     });
 })
