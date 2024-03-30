77487aa95f5d78a00129843fd3d40c17ba5a5f34
amoungui <samoungui@gmail.com>
2024-03-30 21:50:50 +0100

    comment test code of Slider component

--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Slider from "./index";
 import { api, DataProvider } from "../../contexts/DataContext";
 
+// Définition des données de test pour le slider
 const data = {
   focus: [
     {
@@ -26,18 +27,35 @@ const data = {
   ],
 };
 
+// Début de la description des tests pour le composant 'Slider'
 describe("When slider is created", () => {
+
+  // Test spécifique pour vérifier qu'une liste de cartes est affichée
   it("a list card is displayed", async () => {
+
+    // Création d'une fonction mock pour console.error pour éviter les erreurs non gérées
     window.console.error = jest.fn();
+
+    // Création d'une fonction mock pour l'API de chargement des données qui renvoie les données de test
     api.loadData = jest.fn().mockReturnValue(data);
+
+    // Rendu du composant 'Slider' à l'intérieur du fournisseur de données
     render(
       <DataProvider>
         <Slider />
       </DataProvider>
     );
+
+    // Attente de l'affichage du texte "World economic forum"
     await screen.findByText("World economic forum");
-    const janvierElements = await screen.findAllByText("janvier"); // Utilisez findAllByText pour obtenir tous les éléments avec le texte "février"
-    expect(janvierElements.length).toBeGreaterThan(0); // Vérifiez qu'au moins un élément avec le texte "février" a été trouvé
+
+    // Recherche de tous les éléments avec le texte "janvier"
+    const janvierElements = await screen.findAllByText("janvier");
+
+    // Vérification qu'au moins un élément avec le texte "janvier" a été trouvé
+    expect(janvierElements.length).toBeGreaterThan(0);
+
+    // Attente de l'affichage du texte "Oeuvre à la coopération entre le secteur public et le privé."
     await screen.findByText(
       "Oeuvre à la coopération entre le secteur public et le privé."
     );
